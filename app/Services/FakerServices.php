296cd77aac15fea6b9cdb1d989ee9039f58775e1
ArmaghanKhan <khanhassan918@ymail.com>
2022-05-04 18:51:02 +0500

    ATE-005: more options added

--- app/Services/FakerServices.php
+++ app/Services/FakerServices.php
@@ -5,6 +5,7 @@ namespace App\Services;
 class FakerServices {
 
     private $methods = [
+        // Person
         'title' => 'title',
         'title_male' => 'titleMale',
         'title_female' => 'titleFemale',
@@ -12,7 +13,20 @@ class FakerServices {
         'first_name' => 'firstName',
         'first_name_male' => 'firstNameMale',
         'first_name_female' => 'firstNameFemale',
-        'last_name' => 'lastName'
+        'last_name' => 'lastName',
+
+        // Address
+        'city_prefix' => 'cityPrefix',
+        'state' => 'state',
+        'city' => 'city',
+        'country' => 'country',
+
+        // Phone Number
+        'phone_number' => 'phoneNumber',
+
+        // Company
+        'company' => 'company',
+        'job_title' => 'jobTitle'
     ];
 
     public function getAvailableFunctions(){
